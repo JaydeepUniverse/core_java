ab74c920cca88ef315a51f54c52d93f9b37b2818
JaydeepUniverse <ecjaydeepsoni@gmail.com>
2018-12-27 11:22:22 +0530

    update

--- src/basic/Hello.java
+++ src/basic/Hello.java
@@ -1,8 +1,13 @@
 package basic;
 
+import java.util.Random;
+
 public class Hello {
 
     public static void main(String[] args) {
+	Random rand = new Random(); 
+	int value = rand.nextInt(500); 
+	System.out.println(value);
       System.out.println("Hello Java!!");
       System.out.println("This is my first java program !");
     }
